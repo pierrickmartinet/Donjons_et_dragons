44d8c3f8c833ddae1ac2abf353718bc39a68d3cf
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-04-29 11:26:25 +0200

    Fonctionnalité démarrer ou quitter le jeu #1

--- DonjonEtDragons/src/menu/Menu.java
+++ DonjonEtDragons/src/menu/Menu.java
@@ -3,6 +3,7 @@ package menu;
 
 // Import de la classe scanner permettant de lire les entrées clavier
 import java.util.Scanner;
+//import java.lang.System;
 
 // Import de la classe Warriors dans le package characters
 import characters.Warriors;
@@ -10,23 +11,29 @@ import characters.Warriors;
 
 public class Menu {
 	
-	// Attributs
-	private String firstChoice;
+	// ATTRIBUTS
 	Scanner keyboard = new Scanner(System.in);
 	
 	
 	// Point d'entrée du programme
 	public static void main(String[] args) {
 		
-		// Création d'un objet menu qui permet d'utiliser les méthodes de la class menu
+		// Création d'un objet menu qui permet d'utiliser les méthodes de la classe Menu
 		Menu menu = new Menu();
 		
 		// Phrase de bienvenue du jeu
-		System.out.println("Bonjour, bienvenue sur le jeu Donjons et dragons");
+		System.out.println("Bonjour, bienvenue sur le jeu Donjons et dragons, veuillez saisir start ou exit");
 		
-		// Premier menu: nouveau personnage ou exit
-		System.out.println("Veuillez saisir start ou exit");
-		menu.setFirstChoice(menu.getKeyboard().nextLine());
+		// Enregistrement du premier choix saisie dans la variable firstChoice
+		String firstChoice = menu.getKeyboard().nextLine();
+		
+		// Si le joueur saisi start le jeu démarre sinon le jeu s'arrêtte 
+		if(firstChoice.equals("start")) {
+			System.out.println("C'est partit ! Choisis ton personnage : saisis warrior ou magicien");
+		} else {
+			System.out.println("A bientôt");
+			System.exit(0);
+		}
 		
 		
 		//création d'un Warrior
@@ -35,16 +42,6 @@ public class Menu {
 		// Affichage d'un Warrior
 		System.out.println(thor);
 		
-	}
-
-	// Getters et Setters
-	public String getFirstChoice() {
-		return firstChoice;
-	}
-
-
-	public void setFirstChoice(String firstChoice) {
-		this.firstChoice = firstChoice;
 	}
 
 
