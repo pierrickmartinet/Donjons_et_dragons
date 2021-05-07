64c70c980f8af05e8bf2a2dff3eda4d1d646f16e
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-05-07 13:24:19 +0200

    Feat: fonctionnalité shuffle sur le plateau de jeu #23

--- DonjonEtDragons/src/game/BoardGame.java
+++ DonjonEtDragons/src/game/BoardGame.java
@@ -2,6 +2,7 @@ package game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 // Import cases du plateau et contenue des cases
@@ -44,81 +45,81 @@ public class BoardGame {
 		// case 2
 		boardCases.add(new WeaponWarriorCase(new Mace("massue de guerrier", 3)));
 		// case 3
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 4
 		boardCases.add(new WeaponMagicianCase(new Lightning("eclair de magicien", 2)));
 		// case 5
 		boardCases.add(new WeaponWarriorCase(new Mace("massue de guerrier", 3)));
 		// case 6
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 7
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 8
 		boardCases.add(new WeaponMagicianCase(new Lightning("eclair de magicien", 2)));
 		// case 9
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 10
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 11
 		boardCases.add(new WeaponWarriorCase(new Mace("massue de guerrier", 3)));
 		// case 12
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 13
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 14
 		boardCases.add(new EmptyCase());
 		// case 15
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 16
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 17
 		boardCases.add(new WeaponMagicianCase(new Lightning("eclair de magicien", 2)));
 		// case 18
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 19
 		boardCases.add(new WeaponWarriorCase(new Sword("épée de guerrier", 5)));
 		// case 20
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 21
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 22
 		boardCases.add(new WeaponWarriorCase(new Mace("massue de guerrier", 3)));
 		// case 23
 		boardCases.add(new WeaponMagicianCase(new Lightning("eclair de magicien", 2)));
 		// case 24
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 25
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 26
 		boardCases.add(new WeaponWarriorCase(new Sword("épée de guerrier", 5)));
 		// case 27
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 28
 		boardCases.add(new PotionCase(new BigPotion("grosse potion", 5)));
 		// case 29
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 30
-		boardCases.add(new EnemyCase(new Goblin("Gobelin", 15, 4)));
+		boardCases.add(new EnemyCase(new Goblin("Gobelin", 6, 1)));
 		// case 31
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 32
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 33
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 34
 		boardCases.add(new EmptyCase());
 		// case 35
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 36
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 37
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 38
 		boardCases.add(new WeaponWarriorCase(new Mace("massue de guerrier", 3)));
 		// case 39
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 40
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 41
 		boardCases.add(new PotionCase(new BigPotion("grosse potion", 5)));
 		// case 42
@@ -126,13 +127,13 @@ public class BoardGame {
 		// case 43
 		boardCases.add(new PotionCase(new StandardPotion("potion standard", 2)));
 		// case 44
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 45
 		boardCases.add(new EnemyCase(new Dragon("Dragon", 15, 4)));
 		// case 46
 		boardCases.add(new EmptyCase());
 		// case 47
-		boardCases.add(new EnemyCase(new Wizard("Sorcier", 15, 4)));
+		boardCases.add(new EnemyCase(new Wizard("Sorcier", 9, 2)));
 		// case 48
 		boardCases.add(new WeaponMagicianCase(new Fireball("boule de feu de magicien", 7)));
 		// case 49
@@ -165,6 +166,9 @@ public class BoardGame {
 		boardCases.add(new EnemyCase(new Dragon("Dragon", 15, 4)));
 		// case 63
 		boardCases.add(new EmptyCase());
+		
+		// Mélange de la liste
+		Collections.shuffle(boardCases);
 
 	}
 
